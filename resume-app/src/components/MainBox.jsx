61f41ea7c4569745c3c6e8a0fe61092688c980c8
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2023-09-20 01:13:52 +0530

    Language changes

--- resume-app/src/components/MainBox.jsx
+++ resume-app/src/components/MainBox.jsx
@@ -26,7 +26,7 @@ function MainBox() {
             <div className="flex  items-center justify-center">
               <div className="group relative cursor-pointer border-b border-white-700 p-4  transition ease-in-out duration-500 hover:border-white-100 hover:scale-110">
                 <div className="text-5xl flex items-center relative">
-                  {"Amitabh Suyash".split("").map((letter, i) => (
+                  {"amitabhSuyash".split("").map((letter, i) => (
                     <div
                       key={i}
                       className="text-white origin-top transition-transform duration-300 ease-in-out group-hover:scale-y-0"
@@ -37,7 +37,7 @@ function MainBox() {
                   ))}
                 </div>
                 <div className="text-5xl absolute bottom-0 top-0  flex items-center ">
-                  {"Amitabh Suyash".split("").map((letter, i) => (
+                  {"amitabhSuyash".split("").map((letter, i) => (
                     <div
                       key={i}
                       className="text-white origin-top transition-transform duration-300 ease-in-out scale-y-0 group-hover:scale-y-100"
@@ -58,8 +58,8 @@ function MainBox() {
               className=" text-xl  text-center "
               style={{ color: "lightcyan" }}
             >
-              I'm a Gurgaon based Full Stack Software Engineer, currently
-              working at ZS Associates helping build modern internal tools.
+              I'm a Gurgaon based Full Stack Developer, currently working at ZS
+              Associates helping build modern internal tools.
             </span>
           </Col>
         </Row>
